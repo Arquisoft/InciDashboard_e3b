3b3ba0ae9bfa6bb72870a19b12a1a0acc1bbca02
Manuel Junco <UO252010@uniovi.es>
2018-03-24 20:50:07 +0100

    Login
    Cambiado login para que sea por defecto y evitar posibles intrusos no autenticados. Ahora es obligatorio loguearse para usar la aplicación

--- src/main/java/inciDashboard/controllers/HomeController.java
+++ src/main/java/inciDashboard/controllers/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String indexBarra(Model model) {
-		return "redirect:/home";
+		return "redirect:/login";
 	}
 
 	
