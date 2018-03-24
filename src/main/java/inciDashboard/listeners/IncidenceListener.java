e5f917501b0902de63c10f97cca39741f37c1357
Manuel Junco <UO252010@uniovi.es>
2018-03-24 22:07:22 +0100

    Renombrada clase listener de incidencias
    Renombrada la clase listener que recibe las incidencias de kafka a un nombre más apropiado

--- src/main/java/inciDashboard/listeners/IncidenceListener.java
+++ src/main/java/inciDashboard/listeners/IncidenceListener.java
@@ -7,13 +7,13 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 
 @ManagedBean
-public class MessageListener {
+public class IncidenceListener {
 
-    private static final Logger logger = Logger.getLogger(MessageListener.class);
+    private static final Logger logger = Logger.getLogger(IncidenceListener.class);
 
     @KafkaListener(topics = "incidence")
     public void listen(String data) {
-        logger.info("New message received: \"" + data + "\"");
+        logger.info("New incidence received: \"" + data + "\"");
         //recoger incidencia recibida y tratarla
     }
 
