aa8f90899a3e13c1fe8ed94e0dba808432fcaf73
Kilian Pérez González <kilianpg@gmail.com>
2018-05-22 13:52:36 +0200

    Parametriza el 'topic' de Apache Kafka
    	https://docs.spring.io/spring-kafka/reference/htmlsingle/#kafka-listener-annotation
    	https://github.com/spring-projects/spring-kafka/issues/361
    
    Ver también: Issue #20, Arquisoft/Inci_e3b_modules#5

--- src/main/java/es/uniovi/asw/e3b/incidashboard_e3b/listeners/IncidenceListener.java
+++ src/main/java/es/uniovi/asw/e3b/incidashboard_e3b/listeners/IncidenceListener.java
@@ -5,33 +5,37 @@ import javax.annotation.ManagedBean;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 
 import es.uniovi.asw.e3b.incidashboard_e3b.controllers.IncidenciasController;
 import es.uniovi.asw.e3b.incidashboard_e3b.controllers.MainController;
 import es.uniovi.asw.e3b.incidashboard_e3b.entities.Incidence;
 import es.uniovi.asw.e3b.incidashboard_e3b.util.ParserJsonToIncidence;
 
-
 @ManagedBean
 public class IncidenceListener {
 
-    private static final Logger logger = Logger.getLogger(IncidenceListener.class);
-    
-    @Autowired
-    private MainController mainController;
+	private static final Logger logger = Logger.getLogger(IncidenceListener.class);
 
-    @KafkaListener(topics = "incidences")
-    public void listen(String data) {
-    	mainController.sendNewMessage(data);
-        logger.info("New incidence received: \"" + data + "\"");
+	@Value("${kafka.topic:incidences}")
+	private String kafkaTopic;
 
-        // Recoger incidencia recibida y tratarla // comentado todo... comprobar si es correcto, sino: eliminar comentario
-        Incidence incidencia = ParserJsonToIncidence.JsonToIncidence(new JSONObject(data));
-        IncidenciasController inciController = new IncidenciasController();
-        inciController.recieveIncidence(incidencia);
-    }
+	@Autowired
+	private MainController mainController;
 
+	@KafkaListener(topics = "${kafka.topic:incidences}")
+	public void listen(String data) {
+		logger.info("New incidence received: \"" + data + "\" on topic '" + kafkaTopic + "'");
 
+		mainController.sendNewMessage(data);
+		// Recoger incidencia recibida y tratarla // comentado todo... comprobar si es
+		// correcto, sino: eliminar comentario
+		Incidence incidencia = ParserJsonToIncidence.JsonToIncidence(new JSONObject(data));
+		IncidenciasController inciController = new IncidenciasController();
+		inciController.recieveIncidence(incidencia);
+	}
 
 }
