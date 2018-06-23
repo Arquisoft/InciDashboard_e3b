27feef943dbf9f439326eaab093d6e41a63763bd
Daniel Martinez Valeriano <32536834+Gemeto@users.noreply.github.com>
2018-06-23 22:19:11 +0200

    Anadido findByEmail

--- src/main/java/es/uniovi/asw/e3b/incidashboard_e3b/repositories/AgentsRepository.java
+++ src/main/java/es/uniovi/asw/e3b/incidashboard_e3b/repositories/AgentsRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import es.uniovi.asw.e3b.incidashboard_e3b.entities.Agent;
 
 public interface AgentsRepository extends CrudRepository<Agent,Long>{
+  
+  Agente findByEmail(String email);
 
 }
